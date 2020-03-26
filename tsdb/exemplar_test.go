2516fad5f769f42a354de6850b44ef7c543187e9
Callum Styan <callumstyan@gmail.com>
2020-03-25 19:30:51 -0700

    WIP implementation of single fixed size buffer for all exemplars.
    Signed-off-by: Callum Styan <callumstyan@gmail.com>

--- tsdb/exemplar_test.go
+++ tsdb/exemplar_test.go
@@ -225,3 +225,239 @@ func TestSelectExemplarOrdering(t *testing.T) {
 
 	testutil.Assert(t, reflect.DeepEqual(exemplars[1:], ret), "select did not return all exemplars")
 }
+
+func TestAddExemplar_Circ(t *testing.T) {
+	es := NewCircularExemplarStorage(2)
+
+	l := labels.Labels{
+		{Name: "service", Value: "asdf"},
+	}
+	e := exemplar.Exemplar{
+		Labels: labels.Labels{
+			labels.Label{
+				Name:  "traceID",
+				Value: "qwerty",
+			},
+		},
+		Value: 0.1,
+		HasTs: false,
+	}
+
+	err := es.AddExemplar(l, 0, e)
+	testutil.Ok(t, err)
+	testutil.Equals(t, es.index[l.String()], 0, "exemplar was not stored correctly")
+
+	err = es.AddExemplar(l, 0, e)
+	testutil.NotOk(t, err)
+
+	e2 := exemplar.Exemplar{
+		Labels: labels.Labels{
+			labels.Label{
+				Name:  "traceID",
+				Value: "zxcvb",
+			},
+		},
+		Value: 0.1,
+		HasTs: false,
+	}
+
+	err = es.AddExemplar(l, 0, e2)
+	testutil.Ok(t, err)
+	testutil.Equals(t, es.index[l.String()], 1, "exemplar was not stored correctly")
+}
+
+func TestAddExemplar_CircOverwrite(t *testing.T) {
+	es := NewCircularExemplarStorage(2)
+
+	l1 := labels.Labels{
+		{Name: "service", Value: "asdf"},
+	}
+	l2 := labels.Labels{
+		{Name: "service", Value: "xyz"},
+	}
+	e := exemplar.Exemplar{
+		Labels: labels.Labels{
+			labels.Label{
+				Name:  "traceID",
+				Value: "qwerty",
+			},
+		},
+		Value: 0.1,
+		HasTs: false,
+	}
+	e2 := exemplar.Exemplar{
+		Labels: labels.Labels{
+			labels.Label{
+				Name:  "traceID",
+				Value: "zxcvbn",
+			},
+		},
+		Value: 0.1,
+		HasTs: false,
+	}
+	e3 := exemplar.Exemplar{
+		Labels: labels.Labels{
+			labels.Label{
+				Name:  "traceID",
+				Value: "asdfgh",
+			},
+		},
+		Value: 0.1,
+		HasTs: false,
+	}
+
+	err := es.AddExemplar(l1, 0, e)
+	testutil.Ok(t, err)
+	testutil.Equals(t, es.index[l1.String()], 0, "exemplar was not stored correctly")
+
+	err = es.AddExemplar(l1, 0, e2)
+	testutil.Ok(t, err)
+	testutil.Equals(t, es.index[l1.String()], 1, "exemplar was not stored correctly")
+
+	err = es.AddExemplar(l2, 0, e3)
+	testutil.Ok(t, err)
+	testutil.Equals(t, es.index[l2.String()], 0, "exemplar was not stored correctly")
+
+}
+
+func TestSelectExemplar_Circ(t *testing.T) {
+	es := NewCircularExemplarStorage(3)
+
+	l := labels.Labels{
+		{Name: "service", Value: "asdf"},
+	}
+	exemplars := []exemplar.Exemplar{
+		exemplar.Exemplar{
+			Labels: labels.Labels{
+				labels.Label{
+					Name:  "traceID",
+					Value: "qwerty",
+				},
+			},
+			Value: 0.1,
+			HasTs: false,
+		},
+		exemplar.Exemplar{
+			Labels: labels.Labels{
+				labels.Label{
+					Name:  "traceID",
+					Value: "zxcvbn",
+				},
+			},
+			Value: 0.1,
+			HasTs: false,
+		},
+		exemplar.Exemplar{
+			Labels: labels.Labels{
+				labels.Label{
+					Name:  "traceID",
+					Value: "asdfgh",
+				},
+			},
+			Value: 0.1,
+			HasTs: false,
+		},
+	}
+
+	for i, e := range exemplars {
+		err := es.AddExemplar(l, 0, e)
+		testutil.Ok(t, err)
+		testutil.Equals(t, es.index[l.String()], i, "exemplar was not stored correctly")
+	}
+
+	el, err := es.Select(l)
+	testutil.Ok(t, err)
+	testutil.Assert(t, len(el) == 3, "didn't get expected one exemplar")
+
+	for i := range exemplars {
+		testutil.Assert(t, el[i].Equals(exemplars[i]), "")
+	}
+}
+
+// This is a set of stored exemplars I scraped and stored locally that resulted in an infinite loop.
+// This test ensures Select doesn't infinitely loop on them anymore.
+func TestSelectExemplar_OverwriteLoop(t *testing.T) {
+	es := NewCircularExemplarStorage(10)
+
+	l1 := labels.Labels{
+		{Name: "__name__", Value: "test_metric"},
+		{Name: "service", Value: "asdf"},
+	}
+	l2 := labels.Labels{
+		{Name: "__name__", Value: "test_metric"},
+
+		{Name: "service", Value: "qwer"},
+	}
+
+	es.index[l1.String()] = 0
+	es.exemplars[0] = circularBufferEntry{
+		seriesLabels: l1,
+		prev:         6,
+	}
+	es.exemplars[6] = circularBufferEntry{
+		seriesLabels: l1,
+		prev:         2,
+	}
+
+	es.index[l2.String()] = 2
+	es.exemplars[2] = circularBufferEntry{
+		exemplar: exemplar.Exemplar{
+			Ts: 10,
+		},
+		seriesLabels: l2,
+		prev:         1,
+	}
+	es.exemplars[1] = circularBufferEntry{
+		exemplar: exemplar.Exemplar{
+			Ts: 10,
+		},
+		seriesLabels: l2,
+		prev:         9,
+	}
+	es.exemplars[9] = circularBufferEntry{
+		exemplar: exemplar.Exemplar{
+			Ts: 9,
+		},
+		seriesLabels: l2,
+		prev:         8,
+	}
+	es.exemplars[8] = circularBufferEntry{
+		exemplar: exemplar.Exemplar{
+			Ts: 8,
+		},
+		seriesLabels: l2,
+		prev:         7,
+	}
+	es.exemplars[7] = circularBufferEntry{
+		exemplar: exemplar.Exemplar{
+			Ts: 7,
+		},
+		seriesLabels: l2,
+		prev:         5,
+	}
+	es.exemplars[5] = circularBufferEntry{
+		exemplar: exemplar.Exemplar{
+			Ts: 6,
+		},
+		seriesLabels: l2,
+		prev:         4,
+	}
+	es.exemplars[4] = circularBufferEntry{
+		exemplar: exemplar.Exemplar{
+			Ts: 5,
+		},
+		seriesLabels: l2,
+		prev:         3,
+	}
+	es.exemplars[3] = circularBufferEntry{
+		exemplar: exemplar.Exemplar{
+			Ts: 4,
+		},
+		seriesLabels: l2,
+		prev:         1,
+	}
+
+	el, err := es.Select(l2)
+	testutil.Ok(t, err)
+	testutil.Assert(t, len(el) == 8, "didn't get expected 8 exemplars")
+}
