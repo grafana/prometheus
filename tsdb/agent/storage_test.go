832c7c0125b649c35280f9419db4cf691a0a7e23
SriKrishna Paparaju <paparaju@gmail.com>
2021-10-06 22:07:57 -0400

    changes as per code review comments

--- tsdb/agent/storage_test.go
+++ tsdb/agent/storage_test.go
@@ -15,8 +15,10 @@ package agent
 
 import (
 	"context"
+	"io/ioutil"
 	"os"
-	"path/filepath"
+	"strconv"
+	"sync"
 	"testing"
 	"time"
 
@@ -24,21 +26,25 @@ import (
 
 	"github.com/prometheus/client_golang/prometheus"
 	dto "github.com/prometheus/client_model/go"
-	"github.com/prometheus/common/promlog"
 	"github.com/prometheus/prometheus/pkg/labels"
 	"github.com/prometheus/prometheus/storage/remote"
+	"github.com/prometheus/prometheus/tsdb/record"
 	"github.com/prometheus/prometheus/tsdb/tsdbutil"
+	"github.com/prometheus/prometheus/tsdb/wal"
 	"github.com/stretchr/testify/require"
 )
 
-var promAgentData = filepath.Join(os.TempDir(), "data-agent")
-
 func TestUnsupported(t *testing.T) {
-	opts := DefaultOptions()
-	cfg := promlog.Config{}
-	logger := promlog.New(&cfg)
+	promAgentDir, err := ioutil.TempDir("", "TestUnsupported")
+	require.NoError(t, err)
+	t.Cleanup(func() {
+		require.NoError(t, os.RemoveAll(promAgentDir))
+	})
 
-	s, err := NewStorage(logger, prometheus.DefaultRegisterer, nil, promAgentData, opts)
+	opts := DefaultOptions()
+	logger := log.NewNopLogger()
+
+	s, err := NewStorage(logger, prometheus.DefaultRegisterer, nil, promAgentDir, opts)
 	if err != nil {
 		t.Fatalf("unable to create storage for the agent: %v", err)
 	}
@@ -60,94 +66,114 @@ func TestUnsupported(t *testing.T) {
 	})
 }
 
-func TestPreCommit(t *testing.T) {
-	const numDatapoints = 1000
-
-	// Create 8 series with 1000 data-points of different ranges and run queries.
-	lbls := labelsForTest()
-	opts := DefaultOptions()
-	cfg := promlog.Config{}
-	logger := promlog.New(&cfg)
-	reg := prometheus.NewRegistry()
-
-	s, err := NewStorage(logger, reg, nil, promAgentData, opts)
-	if err != nil {
-		t.Fatalf("unable to create storage for the agent: %v", err)
-	}
-
-	a := s.Appender(context.TODO())
-	for _, l := range lbls {
-		lset := labels.New(l...)
-		for i := 0; i < numDatapoints; i++ {
-			sample := tsdbutil.GenerateSamples(0, 1)
-			_, err := a.Append(0, lset, sample[0].T(), sample[0].V())
-			require.NoError(t, err)
-		}
-	}
-
-	var samplesAdded float64
-	var actvieTimeSeries float64
-
-	m := gatherFamily(t, reg, "prometheus_agent_wal_active_series")
-	actvieTimeSeries = m.Metric[0].Gauge.GetValue()
-
-	m = gatherFamily(t, reg, "prometheus_agent_wal_samples_appended_total")
-	samplesAdded = m.Metric[0].Counter.GetValue()
-
-	require.Equal(t, samplesAdded, float64(numDatapoints*8))
-	require.Equal(t, actvieTimeSeries, float64(8))
-}
-
 func TestCommit(t *testing.T) {
-	const numDatapoints = 1000
+	const (
+		numDatapoints = 1000
+		numSeries     = 8
+	)
 
-	// Create 8 series with 1000 data-points of different ranges and run queries.
-	lbls := labelsForTest()
+	promAgentDir, err := ioutil.TempDir("", t.Name())
+	require.NoError(t, err)
+	t.Cleanup(func() {
+		require.NoError(t, os.RemoveAll(promAgentDir))
+	})
+
+	lbls := labelsForTest(t.Name(), numSeries)
 	opts := DefaultOptions()
-	cfg := promlog.Config{}
-	logger := promlog.New(&cfg)
+	logger := log.NewNopLogger()
 	reg := prometheus.NewRegistry()
-	remoteStorage := remote.NewStorage(log.With(logger, "component", "remote"), reg, startTime, promAgentData, time.Second*30, nil)
+	remoteStorage := remote.NewStorage(log.With(logger, "component", "remote"), reg, startTime, promAgentDir, time.Second*30, nil)
 
-	s, err := NewStorage(logger, reg, remoteStorage, promAgentData, opts)
+	s, err := NewStorage(logger, reg, remoteStorage, promAgentDir, opts)
 	if err != nil {
 		t.Fatalf("unable to create storage for the agent: %v", err)
 	}
 
 	a := s.Appender(context.TODO())
+
 	for _, l := range lbls {
 		lset := labels.New(l...)
+
 		for i := 0; i < numDatapoints; i++ {
 			sample := tsdbutil.GenerateSamples(0, 1)
 			_, err := a.Append(0, lset, sample[0].T(), sample[0].V())
 			require.NoError(t, err)
 		}
+		require.NoError(t, a.Commit())
 	}
 
-	require.NoError(t, a.Commit())
-	time.Sleep(time.Second * 10)
+	// Read records from WAL and check for expected count of series and samples.
+	walSeriesCount := 0
+	walSamplesCount := 0
 
-	var walPages float64
+	reg = prometheus.NewRegistry()
+	remoteStorage = remote.NewStorage(log.With(logger, "component", "remote"), reg, startTime, promAgentDir, time.Second*30, nil)
 
-	m := gatherFamily(t, reg, "prometheus_tsdb_wal_completed_pages_total")
-	walPages = m.Metric[0].Counter.GetValue()
+	s, err = NewStorage(logger, nil, remoteStorage, promAgentDir, opts)
+	if err != nil {
+		t.Fatalf("unable to create storage for the agent: %v", err)
+	}
 
-	require.GreaterOrEqual(t, walPages, float64(1))
+	var dec record.Decoder
+
+	if err == nil {
+		sr, err := wal.NewSegmentsReader(s.wal.Dir())
+		require.NoError(t, err)
+
+		r := wal.NewReader(sr)
+		seriesPool := sync.Pool{
+			New: func() interface{} {
+				return []record.RefSeries{}
+			},
+		}
+		samplesPool := sync.Pool{
+			New: func() interface{} {
+				return []record.RefSample{}
+			},
+		}
+
+		for r.Next() {
+			rec := r.Record()
+			switch dec.Type(rec) {
+			case record.Series:
+				series := seriesPool.Get().([]record.RefSeries)[:0]
+				series, _ = dec.Series(rec, series)
+				walSeriesCount += len(series)
+			case record.Samples:
+				samples := samplesPool.Get().([]record.RefSample)[:0]
+				samples, _ = dec.Samples(rec, samples)
+				walSamplesCount += len(samples)
+			default:
+			}
+		}
+	}
+
+	// Retrieved series count from WAL should match the count of series been added to the WAL.
+	require.Equal(t, walSeriesCount, numSeries)
+
+	// Retrieved samples count from WAL should match the count of samples been added to the WAL.
+	require.Equal(t, walSamplesCount, numSeries*numDatapoints)
 }
 
-func TestTruncateWAL(t *testing.T) {
-	const numDatapoints = 1000
+func TestRollback(t *testing.T) {
+	const (
+		numDatapoints = 1000
+		numSeries     = 8
+	)
 
-	// Create 8 series with 1000 data-points of different ranges and run queries.
-	lbls := labelsForTest()
+	promAgentDir, err := ioutil.TempDir("", t.Name())
+	require.NoError(t, err)
+	t.Cleanup(func() {
+		require.NoError(t, os.RemoveAll(promAgentDir))
+	})
+
+	lbls := labelsForTest(t.Name(), numSeries)
 	opts := DefaultOptions()
-	opts.TruncateFrequency = time.Second * 15
-	cfg := promlog.Config{}
-	logger := promlog.New(&cfg)
+	logger := log.NewNopLogger()
 	reg := prometheus.NewRegistry()
-	remoteStorage := remote.NewStorage(log.With(logger, "component", "remote"), reg, startTime, promAgentData, time.Second*30, nil)
+	remoteStorage := remote.NewStorage(log.With(logger, "component", "remote"), reg, startTime, promAgentDir, time.Second*30, nil)
 
-	s, err := NewStorage(logger, reg, remoteStorage, promAgentData, opts)
+	s, err := NewStorage(logger, reg, remoteStorage, promAgentDir, opts)
 	if err != nil {
 		t.Fatalf("unable to create storage for the agent: %v", err)
 	}
@@ -156,38 +182,195 @@ func TestTruncateWAL(t *testing.T) {
 
 	for _, l := range lbls {
 		lset := labels.New(l...)
+
 		for i := 0; i < numDatapoints; i++ {
 			sample := tsdbutil.GenerateSamples(0, 1)
 			_, err := a.Append(0, lset, sample[0].T(), sample[0].V())
 			require.NoError(t, err)
-
 		}
 	}
 
-	require.NoError(t, a.Commit())
-	time.Sleep(time.Second * 20)
+	require.NoError(t, a.Rollback())
 
-	var deleteSeries float64
+	// Read records from WAL and check for expected count of series and samples.
+	walSeriesCount := 0
+	walSamplesCount := 0
+
+	reg = prometheus.NewRegistry()
+	remoteStorage = remote.NewStorage(log.With(logger, "component", "remote"), reg, startTime, promAgentDir, time.Second*30, nil)
+
+	s, err = NewStorage(logger, nil, remoteStorage, promAgentDir, opts)
+	if err != nil {
+		t.Fatalf("unable to create storage for the agent: %v", err)
+	}
+
+	var dec record.Decoder
+
+	if err == nil {
+		sr, err := wal.NewSegmentsReader(s.wal.Dir())
+		require.NoError(t, err)
+
+		r := wal.NewReader(sr)
+		seriesPool := sync.Pool{
+			New: func() interface{} {
+				return []record.RefSeries{}
+			},
+		}
+		samplesPool := sync.Pool{
+			New: func() interface{} {
+				return []record.RefSample{}
+			},
+		}
+
+		for r.Next() {
+			rec := r.Record()
+			switch dec.Type(rec) {
+			case record.Series:
+				series := seriesPool.Get().([]record.RefSeries)[:0]
+				series, _ = dec.Series(rec, series)
+				walSeriesCount += len(series)
+			case record.Samples:
+				samples := samplesPool.Get().([]record.RefSample)[:0]
+				samples, _ = dec.Samples(rec, samples)
+				walSamplesCount += len(samples)
+			default:
+			}
+		}
+	}
+
+	// Retrieved series count from WAL should be zero.
+	require.Equal(t, walSeriesCount, 0)
+
+	// Retrieved samples count from WAL should be zero.
+	require.Equal(t, walSamplesCount, 0)
+}
+
+func TestFullTruncateWAL(t *testing.T) {
+	const (
+		numDatapoints = 1000
+		numSeries     = 800
+		lastTs        = 500
+	)
+
+	promAgentDir, err := ioutil.TempDir("", t.Name())
+	require.NoError(t, err)
+	t.Cleanup(func() {
+		require.NoError(t, os.RemoveAll(promAgentDir))
+	})
+
+	lbls := labelsForTest(t.Name(), numSeries)
+	opts := DefaultOptions()
+	opts.TruncateFrequency = time.Minute * 2
+	logger := log.NewNopLogger()
+	reg := prometheus.NewRegistry()
+	remoteStorage := remote.NewStorage(log.With(logger, "component", "remote"), reg, startTime, promAgentDir, time.Second*30, nil)
+
+	s, err := NewStorage(logger, reg, remoteStorage, promAgentDir, opts)
+	if err != nil {
+		t.Fatalf("unable to create storage for the agent: %v", err)
+	}
+
+	a := s.Appender(context.TODO())
+
+	for _, l := range lbls {
+		lset := labels.New(l...)
+
+		for i := 0; i < numDatapoints; i++ {
+			_, err := a.Append(0, lset, int64(lastTs), 0)
+			require.NoError(t, err)
+		}
+		require.NoError(t, a.Commit())
+	}
+
+	// Truncate WAL with mint to GC all the samples.
+	s.truncate(lastTs + 1)
 
 	m := gatherFamily(t, reg, "prometheus_agent_wal_deleted_series")
-	deleteSeries = m.Metric[0].Gauge.GetValue()
+	require.Equal(t, float64(numSeries), m.Metric[0].Gauge.GetValue(), "agent wal truncate mismatch of deleted series count")
+}
 
-	require.Equal(t, deleteSeries, float64(8))
+func TestPartialTruncateWAL(t *testing.T) {
+	const (
+		numDatapoints = 1000
+		numSeries     = 800
+	)
+
+	promAgentDir, err := ioutil.TempDir("", t.Name())
+	require.NoError(t, err)
+	t.Cleanup(func() {
+		require.NoError(t, os.RemoveAll(promAgentDir))
+	})
+
+	opts := DefaultOptions()
+	opts.TruncateFrequency = time.Minute * 2
+	logger := log.NewNopLogger()
+	reg := prometheus.NewRegistry()
+	remoteStorage := remote.NewStorage(log.With(logger, "component", "remote"), reg, startTime, promAgentDir, time.Second*30, nil)
+
+	s, err := NewStorage(logger, reg, remoteStorage, promAgentDir, opts)
+	if err != nil {
+		t.Fatalf("unable to create storage for the agent: %v", err)
+	}
+
+	a := s.Appender(context.TODO())
+
+	var lastTs int64
+
+	// Create first batch of 800 series with 1000 data-points with a fixed lastTs as 500.
+	lastTs = 500
+	lbls := labelsForTest(t.Name()+"batch-1", numSeries)
+	for _, l := range lbls {
+		lset := labels.New(l...)
+
+		for i := 0; i < numDatapoints; i++ {
+			_, err := a.Append(0, lset, lastTs, 0)
+			require.NoError(t, err)
+		}
+		require.NoError(t, a.Commit())
+	}
+
+	// Create second batch of 800 series with 1000 data-points with a fixed lastTs as 600.
+	lastTs = 600
+
+	lbls = labelsForTest(t.Name()+"batch-2", numSeries)
+	for _, l := range lbls {
+		lset := labels.New(l...)
+
+		for i := 0; i < numDatapoints; i++ {
+			_, err := a.Append(0, lset, lastTs, 0)
+			require.NoError(t, err)
+		}
+		require.NoError(t, a.Commit())
+	}
+
+	// Truncate WAL with mint to GC only the first batch of 800 series and retaining 2nd batch of 800 series.
+	s.truncate(lastTs - 1)
+
+	m := gatherFamily(t, reg, "prometheus_agent_wal_deleted_series")
+	require.Equal(t, m.Metric[0].Gauge.GetValue(), float64(numSeries), "agent wal truncate mismatch of deleted series count")
 }
 
 func TestWALReplay(t *testing.T) {
-	const numDatapoints = 1000
+	const (
+		numDatapoints = 1000
+		numSeries     = 8
+		lastTs        = 500
+	)
 
-	// Create 8 series with 1000 data-points of different ranges and run queries.
-	lbls := labelsForTest()
+	promAgentDir, err := ioutil.TempDir("", t.Name())
+	require.NoError(t, err)
+	t.Cleanup(func() {
+		require.NoError(t, os.RemoveAll(promAgentDir))
+	})
+
+	lbls := labelsForTest(t.Name(), numSeries)
 	opts := DefaultOptions()
 
-	cfg := promlog.Config{}
-	logger := promlog.New(&cfg)
+	logger := log.NewNopLogger()
 	reg := prometheus.NewRegistry()
-	remoteStorage := remote.NewStorage(log.With(logger, "component", "remote"), reg, startTime, promAgentData, time.Second*30, nil)
+	remoteStorage := remote.NewStorage(log.With(logger, "component", "remote"), reg, startTime, promAgentDir, time.Second*30, nil)
 
-	s, err := NewStorage(logger, reg, remoteStorage, promAgentData, opts)
+	s, err := NewStorage(logger, reg, remoteStorage, promAgentDir, opts)
 	if err != nil {
 		t.Fatalf("unable to create storage for the agent: %v", err)
 	}
@@ -196,81 +379,56 @@ func TestWALReplay(t *testing.T) {
 
 	for _, l := range lbls {
 		lset := labels.New(l...)
-		for i := 0; i < numDatapoints; i++ {
-			sample := tsdbutil.GenerateSamples(0, 1)
-			_, err := a.Append(0, lset, sample[0].T(), sample[0].V())
-			require.NoError(t, err)
 
+		for i := 0; i < numDatapoints; i++ {
+			_, err := a.Append(0, lset, lastTs, 0)
+			require.NoError(t, err)
 		}
 	}
 
 	require.NoError(t, a.Commit())
+
 	restartOpts := DefaultOptions()
-	restartCfg := promlog.Config{}
-	restartLogger := promlog.New(&restartCfg)
+	restartLogger := log.NewNopLogger()
 	restartReg := prometheus.NewRegistry()
 
-	_, err = NewStorage(restartLogger, restartReg, nil, promAgentData, restartOpts)
+	s, err = NewStorage(restartLogger, restartReg, nil, promAgentDir, restartOpts)
 	if err != nil {
 		t.Fatalf("unable to create storage for the agent: %v", err)
 	}
 
-	var actvieTimeSeries float64
+	// Check if all the series are retrieved back from the WAL.
+	m := gatherFamily(t, restartReg, "prometheus_agent_wal_active_series")
+	require.Equal(t, float64(numSeries), m.Metric[0].Gauge.GetValue(), "agent wal replay mismatch of active series count")
 
-	m := gatherFamily(t, reg, "prometheus_agent_wal_active_series")
-	actvieTimeSeries = m.Metric[0].Gauge.GetValue()
-
-	require.Equal(t, actvieTimeSeries, float64(8))
+	// Check if lastTs of the samples retrieved from the WAL is retained.
+	metrics := s.series.series
+	for i := 0; i < len(metrics); i++ {
+		mp := metrics[i]
+		for _, v := range mp {
+			require.Equal(t, v.lastTs, int64(lastTs))
+		}
+	}
 }
 
 func startTime() (int64, error) {
 	return time.Now().Unix() * 1000, nil
 }
 
-func labelsForTest() []labels.Labels {
-	// Create 8 series with 1000 data-points of different ranges and run queries.
-	return []labels.Labels{
-		{
-			{Name: "a", Value: "b"},
-			{Name: "instance", Value: "localhost:9090"},
+// Create series for tests.
+func labelsForTest(lName string, seriesCount int) []labels.Labels {
+	var series []labels.Labels
+
+	for i := 0; i < seriesCount; i++ {
+		lset := labels.Labels{
+			{Name: "a", Value: lName},
 			{Name: "job", Value: "prometheus"},
-		},
-		{
-			{Name: "a", Value: "b"},
-			{Name: "instance", Value: "127.0.0.1:9090"},
-			{Name: "job", Value: "prometheus"},
-		},
-		{
-			{Name: "a", Value: "b"},
-			{Name: "instance", Value: "127.0.0.1:9090"},
-			{Name: "job", Value: "prom-k8s"},
-		},
-		{
-			{Name: "a", Value: "b"},
-			{Name: "instance", Value: "localhost:9090"},
-			{Name: "job", Value: "prom-k8s"},
-		},
-		{
-			{Name: "a", Value: "c"},
-			{Name: "instance", Value: "localhost:9090"},
-			{Name: "job", Value: "prometheus"},
-		},
-		{
-			{Name: "a", Value: "c"},
-			{Name: "instance", Value: "127.0.0.1:9090"},
-			{Name: "job", Value: "prometheus"},
-		},
-		{
-			{Name: "a", Value: "c"},
-			{Name: "instance", Value: "127.0.0.1:9090"},
-			{Name: "job", Value: "prom-k8s"},
-		},
-		{
-			{Name: "a", Value: "c"},
-			{Name: "instance", Value: "localhost:9090"},
-			{Name: "job", Value: "prom-k8s"},
-		},
+			{Name: "instance", Value: "localhost" + strconv.Itoa(i)},
+		}
+		series = append(series, lset)
 	}
+
+	return series
 }
 
 func gatherFamily(t *testing.T, reg prometheus.Gatherer, familyName string) *dto.MetricFamily {
@@ -286,5 +444,6 @@ func gatherFamily(t *testing.T, reg prometheus.Gatherer, familyName string) *dto
 	}
 
 	t.Fatalf("could not find family %s", familyName)
+
 	return nil
 }
