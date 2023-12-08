56dc1cb56751cd5d7224c7a15fed3bd035f0977b
Owen Williams <owen.williams@grafana.com>
2023-12-08 12:46:26 -0500

    Start trying to write tests for promql

--- promql/parser/parse_test.go
+++ promql/parser/parse_test.go
@@ -472,6 +472,23 @@ var testExpr = []struct {
 		},
 	},
 	{
+		input: ` +{"some_metric"}`,
+		expected: &UnaryExpr{
+			Op: ADD,
+			Expr: &VectorSelector{
+				Name: "some_metric",
+				LabelMatchers: []*labels.Matcher{
+					MustLabelMatcher(labels.MatchEqual, model.MetricNameLabel, "some_metric"),
+				},
+				PosRange: posrange.PositionRange{
+					Start: 2,
+					End:   17,
+				},
+			},
+			StartPos: 1,
+		},
+	},
+	{
 		input:  "",
 		fail:   true,
 		errMsg: "no expression found in input",
@@ -1694,6 +1711,20 @@ var testExpr = []struct {
 				MustLabelMatcher(labels.MatchEqual, model.MetricNameLabel, "foo:bar"),
 			},
 			PosRange: PositionRange{
+				Start: 0,
+				End:   15,
+			},
+		},
+	},
+	{
+		input: `{"foo", a="bc"}`,
+		expected: &VectorSelector{
+			Name: "foo:bar",
+			LabelMatchers: []*labels.Matcher{
+				MustLabelMatcher(labels.MatchEqual, model.MetricNameLabel, "foo"),
+				MustLabelMatcher(labels.MatchEqual, "a", "bc"),
+			},
+			PosRange: posrange.PositionRange{
 				Start: 0,
 				End:   15,
 			},
