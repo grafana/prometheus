07436525ba0a0a2d162eed211f07c3c55c00a343
Sam DeHaan <sam.dehaan@grafana.com>
2025-04-22 15:02:48 -0400

    Add staleness disabling for v3.3.0

--- scrape/manager.go
+++ scrape/manager.go
@@ -370,3 +370,16 @@ func (m *Manager) TargetsDroppedCounts() map[string]int {
 	}
 	return counts
 }
+
+// DisableEndOfRunStalenessMarkers disables the end-of-run staleness markers for the provided targets in the given
+// targetSet. When the end-of-run staleness is disabled for a target, when it goes away, there will be no staleness
+// markers written for its series.
+func (m *Manager) DisableEndOfRunStalenessMarkers(targetSet string, targets []*Target) {
+	m.mtxScrape.Lock()
+	sp, ok := m.scrapePools[targetSet]
+	m.mtxScrape.Unlock()
+
+	if ok {
+		sp.disableEndOfRunStalenessMarkers(targets)
+	}
+}
